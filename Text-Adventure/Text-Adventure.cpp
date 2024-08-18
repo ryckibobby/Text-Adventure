98971173511b14b61ba270897d204841712a58bd
Rycki Harding <ryckiharding@gmail.com>
2024-08-17 23:23:23 -0400

    intro more abilities & challenges

--- Text-Adventure/Text-Adventure.cpp
+++ Text-Adventure/Text-Adventure.cpp
@@ -24,7 +24,7 @@ int main()
 	cin >> abilityChoice;
 
 	while (abilityChoice < 1 || abilityChoice > 3) {
-		cout << "Invalid choice. Please choose a number between 1 and 3: ";
+		cout << "Invalid choice. Please choose a number between 1 and 6: ";
 		cin >> abilityChoice;
 
 	}
@@ -36,8 +36,11 @@ int main()
 void displayAbilities() {
 	cout << "\nAvailiable Abilities:" << endl;
 	cout << "1. Strength - Overcome physical obstacles. " << endl;
-	cout << "2. Intelligence - Solve complex problems" << endl;
-	cout << "3. Agility - Evade traps and hazards" << endl;
+	cout << "2. Intelligence - Solve complex problems." << endl;
+	cout << "3. Agility - Evade traps and hazards." << endl;
+	cout << "4. Charisma - Persuade others and gain abilites." << endl;
+	cout << "5. Stealth - sneak past enemies unnoticed." << endl;
+	cout << "6. Magic - Use spells to alter reality." << endl;
 }
 
 void encounterChallenge(int abilityChoice) {
@@ -57,8 +60,24 @@ void encounterChallenge(int abilityChoice) {
 			cout << "You come across a series of dangerous traps on the ground." << endl;
 			cout << "With your quick reflexes, you dodge and leap over the traps, safely making it through." << endl;
 			break;
+		case 4:
+			cout << "\nYou chose Charisma!" << endl;
+			cout << "You encounter a group of bandits blocking your path." << endl;
+			cout << "With your charm and persuasive skills, you convince them to let you pass without a fight." << endl;
+			break;
+		case 5:
+			cout << "\nYou chose Stealth!" << endl;
+			cout << "You need to infiltrate an enemy camp to retreive a stolen item." << endl;
+			cout << "By using your stealth abilites, you sneak past the guards and retreive the item without being detected." << endl;
+			break;
+		case 6:
+			cout << "\You chose Magic" << endl;
+			cout << "You are faced with a fiery chasm that is too wide to cross." << endl;
+			cout << "Using your magical powers, you cast a spell to create a bridge over the chasm, allowing you to cross safely." << endl;
+			break;
 		default:
 			cout << "An error occurred. Please restart the game." << endl;
 			break;
 	}
+
 }
