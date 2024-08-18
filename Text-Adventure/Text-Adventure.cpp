d493f04d59ae607b390afd5dd712eb960d6b7d65
Rycki Harding <ryckiharding@gmail.com>
2024-08-17 23:01:40 -0400

    intro abilities & challenge function

--- Text-Adventure/Text-Adventure.cpp
+++ Text-Adventure/Text-Adventure.cpp
@@ -1,20 +1,64 @@
-// Text-Adventure.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+void displayAbilities();
+void encounterChallenge(int abilityChoice);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+	int abilityChoice;
+
+	//introduction
+	cout << "Welcome, brave hero! Your quest is to overcome challenges by choosing the right abilities" << endl;
+	cout << "You will be presented with a series of challenges. Choose your abilities wisely!" << endl;
+
+	//display the abilities menu
+	displayAbilities();
+
+	cout << "Enter the number of the ability you choose (1-3): ";
+	cin >> abilityChoice;
+
+	while (abilityChoice < 1 || abilityChoice > 3) {
+		cout << "Invalid choice. Please choose a number between 1 and 3: ";
+		cin >> abilityChoice;
+
+	}
+	encounterChallenge(abilityChoice);
+
+	return 0;
+
+}
+void displayAbilities() {
+	cout << "\nAvailiable Abilities:" << endl;
+	cout << "1. Strength - Overcome physical obstacles. " << endl;
+	cout << "2. Intelligence - Solve complex problems" << endl;
+	cout << "3. Agility - Evade traps and hazards" << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void encounterChallenge(int abilityChoice) {
+	switch (abilityChoice) {
+		case 1:
+			cout << "\nYou chose Strength!" << endl;
+			cout << "You face a giant boulder blocking your path" << endl;
+			cout << "With your immense strength, you push the boulder aside and continue on your way." << endl;
+			break;
+		case 2:
+			cout << "You chose Intelligence!" << endl;
+			cout << "You encounter a locked door with a complex puzzle." << endl;
+			cout << "Using your sharp mind, you solve the puzzle amd the door opens, allowing you to proceed." << endl;
+			break;
+		case 3:
+			cout << "\nYou chose Agility!" << endl;
+			cout << "You come across a series of dangerous traps on the ground." << endl;
+			cout << "With your quick reflexes, you dodge and leap over the traps, safely making it through." << endl;
+			break;
+		default:
+			cout << "An error occurred. Please restart the game." << endl;
+			break;
+	}
+}
